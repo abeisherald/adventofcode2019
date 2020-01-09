b4b8336b1320184a025527e3141e17d2d79cfe57
Abe M <kinofevil@gmail.com>
2020-01-09 20:14:54 +0000

    heck

---
+++ day1.py
@@ -0,0 +1,13 @@
+#import input
+
+list_of_modules = []
+module_fuel_requirements = {}
+
+fileinput = open('day1_input.txt', 'r')
+fuel_modules = fileinput.readlines()
+for module in fuel_modules:
+    list_of_modules.append(module)
+    
+
+
+
