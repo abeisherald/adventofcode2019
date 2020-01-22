79d5fe64fad004072641e48e85efc773a4400188
Abe M <kinofevil@gmail.com>
2020-01-22 22:17:02 +0000

    still buggy

--- day5.py
+++ day5.py
@@ -27,38 +27,41 @@ def compumagic():
         inputindex2 = intprogram[pointer + 2]
         outputindex = intprogram[pointer + 3]
         
+        # 0=position mode, 1=immediate mode
         if opcode in ('1', '2', '5', '6', '7', '8'):
             parameter_1_mode = intprogram[inputindex1] if parameter_1 == '0' else inputindex1
+            parameter_1_mode_index2 = intprogram[inputindex2] if parameter_1 == '0' else inputindex2
             parameter_2_mode = intprogram[inputindex2] if parameter_2 == '0' else inputindex2
+            parameter_2_mode_index1 = intprogram[inputindex1] if parameter_2 == '0' else inputindex1
 
-        # 0=position mode, 1=immediate mode
+        
         if opcode in ('1', '2'):
             f = add if opcode == '1' else mul
             intprogram[outputindex] = f(parameter_1_mode, parameter_2_mode)
             pointer += 4
-        if opcode == '3':
+        elif opcode == '3':
             input_value = int(input(f'Provide a single integer: '))
             outputindex = intprogram[pointer + 1]
             intprogram[outputindex] = input_value
             pointer += 2
-        if opcode == '4':
+        elif opcode == '4':
             opcode_4_output = intprogram[pointer + 1] if parameter_1 == '0' else (pointer + 1)
             output = intprogram[opcode_4_output]
             print(f'The output is {output}.')
             pointer += 2
-        if opcode == '5':
-            pointer = parameter_2_mode if parameter_1 != '0' else pointer
-            x = 3 if parameter_1 != '0' else 0
-            pointer += x
-        if opcode == '6':
-            pointer = parameter_2_mode if parameter_1 == '0' else pointer
+        elif opcode == '5':
+            pointer = parameter_1_mode_index2 if parameter_1 != '0' else pointer
             x = 3 if parameter_1 == '0' else 0
             pointer += x
-        if opcode == '7':
+        elif opcode == '6':
+            pointer = parameter_1_mode_index2 if parameter_1 == '0' else pointer
+            x = 3 if parameter_1 != '0' else 0
+            pointer += x
+        elif opcode == '7':
             x = 1 if parameter_1_mode < parameter_2_mode else 0
             intprogram[outputindex] = x
             pointer += 4
-        if opcode == '8':
+        elif opcode == '8':
             x = 1 if parameter_1_mode == parameter_2_mode else 0
             intprogram[outputindex] = x
             pointer += 4
